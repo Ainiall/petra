994bf5a2b60cae9c1f35cc4481cfd96b44650d37
Ainiall <angela_l.lopez@hotmail.com>
2021-10-08 20:33:01 +0200

    server command

--- commands/server.js
+++ commands/server.js
@@ -1,12 +1,25 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-
+const { SlashCommandBuilder } = require(`@discordjs/builders`);
+const { Permissions, MessageEmbed } = require('discord.js');
+const Discord = require(`discord.js`);
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("server")
-    .setDescription("Display info about this server."),
+    .setName(`server`)
+    .setDescription(`Muestra información sobre el servidor.`),
   async execute(interaction) {
-    return interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );
+    console.log(interaction.guild.memberCount);
+    if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)){
+      const embed = new MessageEmbed()
+      .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
+      .setAuthor(interaction.guild.name, interaction.guild.iconURL({ dynamic: true }))
+      .addField(`ID`, interaction.guild.id, true)
+      .addField(`Creado el`, interaction.guild.joinedAt.toDateString(), true)
+      .addField(`Miembros`, interaction.guild.memberCount.toString(), true)
+      .addField(`Dueño del Servidor`, `<@520278389370912809>`, true)
+      .addField(`Programadores`, `<@148515656927543297><@575724048097476628>`, true)
+      .addField(`Ayudante`,`<@218083715501260811>`,true)
+      .setColor(0x01a330);
+      
+    return interaction.reply({ embeds: [embed] }); 
+    }
   },
 };
